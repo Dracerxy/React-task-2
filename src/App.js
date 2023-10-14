801055b7a90958d850aaa52a47d41715c78a78d6
Sri Ganesan M <sriganesan06@gmail.com>
2023-10-14 16:28:37 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import './style.css';
+import ResultComponent from './components/result.js';
+import KeyPadComponent from './components/keys.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+  state = {
+    result: ""
+  }
+
+  onClick = button => {
+    if(button === "=") {
+      this.calculate();
+    }
+
+    else if(button === "C") {
+      this.reset();
+    }
+
+    else if(button === "CE") {
+      this.backspace();
+    }
+
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  };
+
+  calculate = () => {
+    var checkResult = ''
+    if(this.state.result.includes('--')) {
+      checkResult = this.state.result.replace('--', '+')
+    } else {
+      checkResult = this.state.result;
+    }
+
+    try {
+      this.setState({
+        result: (eval(checkResult) || "") + ""
+      })
+    } catch(e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  backspace = () => {
+    this.setState({
+        result: this.state.result.slice(0, -1)
+    })
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <div>
+          <div className="calculator-body">
+            <ResultComponent result={this.state.result} />
+            <KeyPadComponent onClick={this.onClick} />
+          </div>
+        </div>
       </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
